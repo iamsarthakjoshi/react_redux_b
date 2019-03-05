b92e2a501e9778a6b98a178b5eb0a4f3ad96ca97
Sarthak Joshi <iamsarthakjoshi@gmail.com>
2019-03-05 18:14:09 +1100

    Building Context with JSX

---
+++ jsx/src/index.js
@@ -0,0 +1,23 @@
+// Import the React and ReactDOM Libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+const App = () => {
+    const labelText = 'Enter Name:';
+    const buttonText = {text: 'Click here'};
+    
+     return ( 
+     <div>
+         <label className="label" htmlFor="name">{labelText}</label>
+         <input id="name" type="text"/>
+         <button style={{ backgroundColor: 'grey', color: '#222222'}}>{buttonText.text}</button>
+     </div>
+     );
+};
+
+// Take the react component and display it on the screen 
+ReactDOM.render(
+    <App/>,
+    document.querySelector('#root')
+);
