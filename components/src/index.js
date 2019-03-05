b5cf6984f02abb92e86e8fc8fe7d1403e1e46a85
Sarthak Joshi <iamsarthakjoshi@gmail.com>
2019-03-06 00:07:48 +1100

    Component reuse, showing custom children

--- components/src/index.js
+++ components/src/index.js
@@ -3,31 +3,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // Create a react component
 const App = () => {
      return ( 
         <div className="ui container comments">
-            {/* Provide props from parent(<CommentDetail .. /> jsx element) to child */}
-            {/* author, timeAgo is a prop here, it can be named anything meaningful, Sam is a value of the prop, key-value */}
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 4:45PM" 
-                comment="Nice blog post!" 
-                avatar={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author="Sarthak" 
-                timeAgo="Today at 2:55PM" 
-                comment="Nice blog post!" 
-                avatar={faker.image.avatar()} 
-            />
-            <CommentDetail 
-                author="Joshi" 
-                timeAgo="Today at 3:40PM" 
-                comment="Nice blog post!" 
-                avatar={faker.image.avatar()} 
-            />
+            {/* Also, <CommentDetails acts as a prop/child component for <ApprovalCard>, since proving some config to approvalcard component */}
+            <ApprovalCard>
+                {/* Provide props from parent(<CommentDetail .. /> jsx element) to child */}
+                {/* author, timeAgo is a prop here, it can be named anything meaningful, Sam is a value of the prop, key-value */}
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45PM" 
+                    comment="Nice blog post!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard> 
+                <CommentDetail 
+                    author="Sarthak" 
+                    timeAgo="Today at 2:55PM" 
+                    comment="Nice blog post!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Joshi" 
+                    timeAgo="Today at 3:40PM" 
+                    comment="Nice blog post!" 
+                    avatar={faker.image.avatar()} 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard> 
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
         </div>
      );
 };
