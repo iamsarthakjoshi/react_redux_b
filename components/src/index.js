710714dda8f191f26a216f111afbc6a35919fe19
Sarthak Joshi <iamsarthakjoshi@gmail.com>
2019-03-05 23:38:20 +1100

    Passing props(properties) from parent to child

--- components/src/index.js
+++ components/src/index.js
@@ -2,13 +2,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import ComponentDetail from './ComponentDetail';
+import CommentDetail from './CommentDetail';
 
 // Create a react component
 const App = () => {
      return ( 
         <div className="ui container comments">
-            <ComponentDetail />
+            {/* Provide props from parent(<CommentDetail .. /> jsx element) to child */}
+            {/* author, timeAgo is a prop here, it can be named anything meaningful, Sam is a value of the prop, key-value */}
+            <CommentDetail 
+                author="Sam" 
+                timeAgo="Today at 4:45PM" 
+                comment="Nice blog post!" 
+                avatar={faker.image.avatar()} 
+            />
+            <CommentDetail 
+                author="Sarthak" 
+                timeAgo="Today at 2:55PM" 
+                comment="Nice blog post!" 
+                avatar={faker.image.avatar()} 
+            />
+            <CommentDetail 
+                author="Joshi" 
+                timeAgo="Today at 3:40PM" 
+                comment="Nice blog post!" 
+                avatar={faker.image.avatar()} 
+            />
         </div>
      );
 };
