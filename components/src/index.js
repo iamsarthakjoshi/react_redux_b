fcba1328e03535b40e73c367d287bf9e35ae3015
Sarthak Joshi <iamsarthakjoshi@gmail.com>
2019-03-05 23:18:21 +1100

    React's Props System

--- components/src/index.js
+++ components/src/index.js
@@ -2,25 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import ComponentDetail from './ComponentDetail';
 
 // Create a react component
 const App = () => {
      return ( 
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sarthak
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <ComponentDetail />
         </div>
      );
 };
